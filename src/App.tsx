381ccffc832dee7a16bd968803528d92e59a0108
sribnyi.k <sribnyi.k@gmail.com>
2022-10-12 16:42:10 +0300

    create Main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+
 import './App.css';
+import MainPage from "./MainPage/MainPage";
 
 const App = () =>{
   return (
     <div className="App">
+        <Routes>
+            <Route path="/" element={<MainPage/>}/>
+        </Routes>
     </div>
   )
 }
