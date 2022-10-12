1f3a431627c06ef6a71f15ba1776a913abd1ed9c
ksribnyi <102962195+ksribnyi@users.noreply.github.com>
2022-10-12 21:32:49 +0300

    Revert "Login page"

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
-import MainPage from "./MainPage/MainPage";
-import LoginPage from "./LoginPage/LoginPage";
 import './App.css';
 
 const App = () =>{
   return (
     <div className="App">
-        <Routes>
-            <Route path="/" element={<MainPage/>}/>
-            <Route path="/login" element={<LoginPage/>}/>
-        </Routes>
     </div>
   )
 }
