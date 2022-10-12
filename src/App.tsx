753c332d848bcb7b885d877fffc1249c996bd143
sribnyi.k <sribnyi.k@gmail.com>
2022-10-12 22:10:33 +0300

    create Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import MainPage from "./MainPage/MainPage";
+import LoginPage from "./LoginPage/LoginPage";
 import './App.css';
 
 const App = () =>{
   return (
     <div className="App">
+        <Routes>
+            <Route path="/" element={<MainPage/>}/>
+            <Route path="/login" element={<LoginPage/>}/>
+        </Routes>
     </div>
   )
 }
